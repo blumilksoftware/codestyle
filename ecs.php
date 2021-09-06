dfab00d2c0e6f2748b4bfea17d0b35ff2b6b891b
Krzysztof Rewak <krzysztof.rewak@blumilk.pl>
2021-09-06 18:59:02 +0200

    #30 - adding ecs.php to checked files too

--- ecs.php
+++ ecs.php
@@ -6,7 +6,7 @@ use Blumilk\Codestyle\Config;
 use Blumilk\Codestyle\Configuration\Defaults\Paths;
 
 $config = new Config(
-    paths: new Paths("src", "tests/unit", "tests/codestyle/CodestyleTest.php")
+    paths: new Paths("ecs.php", "src", "tests/unit", "tests/codestyle/CodestyleTest.php", "tests/codestyle/config.php")
 );
 
 return $config->config();
