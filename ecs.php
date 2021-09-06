ccf0ff67e7efbdcfbcf554b3b2a727e19b4285a0
Krzysztof Rewak <krzysztof.rewak@blumilk.pl>
2021-09-06 18:59:50 +0200

    #30 - ecsf

--- ecs.php
+++ ecs.php
@@ -6,7 +6,7 @@ use Blumilk\Codestyle\Config;
 use Blumilk\Codestyle\Configuration\Defaults\Paths;
 
 $config = new Config(
-    paths: new Paths("ecs.php", "src", "tests/unit", "tests/codestyle/CodestyleTest.php", "tests/codestyle/config.php")
+    paths: new Paths("ecs.php", "src", "tests/unit", "tests/codestyle/CodestyleTest.php", "tests/codestyle/config.php"),
 );
 
 return $config->config();
