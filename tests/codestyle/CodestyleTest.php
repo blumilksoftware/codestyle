7ea5246aec313e64ecc60c26cd7465cc8b7447d9
Krzysztof Rewak <krzysztof.rewak@blumilk.pl>
2021-12-08 12:21:42 +0100

    #42 - enums fixed with SwitchCaseSemicolonToColonFixer update

--- tests/codestyle/CodestyleTest.php
+++ tests/codestyle/CodestyleTest.php
@@ -38,7 +38,7 @@ class CodestyleTest extends TestCase
         $result = 0;
         $output = null;
 
-        exec("./vendor/bin/composer " . $command, $output, $result);
+        exec("./vendor/bin/composer " . $command . " 2> /dev/null", $output, $result);
 
         return $result === 0;
     }
@@ -49,6 +49,7 @@ class CodestyleTest extends TestCase
     protected function testFixture(string $name): void
     {
         copy(__DIR__ . "/fixtures/${name}/actual.php", __DIR__ . "/tmp/${name}.php");
+
         $this->assertFalse($this->runComposerEcsCommand());
         $this->assertTrue($this->runComposerEcsCommand(true));
         $this->assertFileEquals(__DIR__ . "/fixtures/${name}/expected.php", __DIR__ . "/tmp/${name}.php");
---
+++ tests/codestyle/fixtures/enums/actual.php
@@ -0,0 +1,7 @@
+<?php
+
+enum Status: string
+{
+    case ACTIVE = "active";
+    case INACTIVE = "inactive";
+}
