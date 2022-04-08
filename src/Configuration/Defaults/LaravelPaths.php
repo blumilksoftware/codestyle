f4aa6ba70373b34ee7c2619035ffa42aa2853644
kamilpiech97 <kamil.piech@blumilk.pl>
2022-04-08 09:47:39 +0200

    #55 - cs fix

--- src/Configuration/Defaults/LaravelPaths.php
+++ src/Configuration/Defaults/LaravelPaths.php
@@ -7,7 +7,6 @@ namespace Blumilk\Codestyle\Configuration\Defaults;
 class LaravelPaths extends Paths
 {
     public const LARAVEL_8_PATHS = ["app", "bootstrap/app.php", "config", "database", "public/index.php", "resources/lang", "routes", "tests"];
-
     public const LARAVEL_9_PATHS = ["app", "bootstrap/app.php", "config", "database", "lang", "public/index.php", "routes", "tests"];
 
     public function __construct(array $paths = self::LARAVEL_9_PATHS)
