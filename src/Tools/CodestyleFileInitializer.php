3df320c4500910f8a960157e48220dd6fadd828e
Krzysztof Rewak <krzysztof.rewak@blumilk.pl>
2022-04-21 13:00:57 +0200

    #60 - bin executables (#63)
    * #60 - executable helper for package init
    
    * #60 - tests fix
    
    * #60 - composer.json empty line at EOF

---
+++ src/Tools/CodestyleFileInitializer.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blumilk\Codestyle\Tools;
+
+class CodestyleFileInitializer
+{
+    public function run(string $root, string $filename = "codestyle.php"): void
+    {
+        $codestyleFilePath = "${root}/${filename}";
+
+        if (file_exists($codestyleFilePath)) {
+            fwrite(STDERR, "File codestyle.php already exists.\n");
+        } else {
+            copy(__DIR__ . "/../../bin/codestyle.stub", $codestyleFilePath);
+            fwrite(STDOUT, "File codestyle.php has been created.\n");
+        }
+    }
+}
---
+++ src/Tools/ComposerManifestScriptsInitializer.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blumilk\Codestyle\Tools;
+
+class ComposerManifestScriptsInitializer
+{
+    public function run(string $root): void
+    {
+        $composerManifestFile = "${root}/composer.json";
+
+        $hasComposerFileChanged = false;
+        $composer = json_decode(file_get_contents($composerManifestFile), associative: true);
+        $scripts = [
+            "cs" => "./vendor/bin/php-cs-fixer fix --dry-run --diff --config codestyle.php",
+            "csf" => "./vendor/bin/php-cs-fixer fix --diff --config codestyle.php",
+        ];
+
+        foreach ($scripts as $command => $script) {
+            if (isset($composer["scripts"][$command])) {
+                fwrite(STDOUT, "Script ${command} is already declared.\n");
+            } else {
+                $composer["scripts"][$command] = $script;
+                fwrite(STDOUT, "Script ${command} has been added to composer.json file.\n");
+                $hasComposerFileChanged = true;
+            }
+        }
+
+        if ($hasComposerFileChanged) {
+            $content = json_encode($composer, flags: JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+            $content = str_replace("    ", "  ", $content);
+            file_put_contents($composerManifestFile, $content);
+        }
+    }
+}
