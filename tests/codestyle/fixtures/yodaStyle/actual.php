206bd6b0a328c9778b65ea358356de84b9f10e38
Kamil Piech <kamil.piech@blumilk.pl>
2022-05-13 10:39:54 +0200

    #69 - added non-yoda style fixer (#72)
    * #69 - added yoda style fixer
    
    * #69 - added more test cases

---
+++ tests/codestyle/fixtures/yodaStyle/actual.php
@@ -0,0 +1,25 @@
+<?php
+
+if (null === $a) {
+    echo "null";
+}
+
+if (null == $a) {
+    echo "null";
+}
+
+if (3 >= $a) {
+    echo 3;
+}
+
+if (3 <= $a) {
+    echo 3;
+}
+
+if (5 > $a) {
+    echo 5;
+}
+
+if (null < $a) {
+    echo "null";
+}
