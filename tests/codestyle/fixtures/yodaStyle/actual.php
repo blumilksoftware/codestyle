3f7f2c5535e87322f7bf52d2dbf0c05fb24af864
kamilpiech97 <kamil.piech@blumilk.pl>
2022-05-13 09:53:59 +0200

    #69 - added more test cases

--- tests/codestyle/fixtures/yodaStyle/actual.php
+++ tests/codestyle/fixtures/yodaStyle/actual.php
@@ -3,3 +3,23 @@
 if (null === $a) {
     echo "null";
 }
+
+if (null == $a) {
+    echo "null";
+}
+
+if (3 >= $a) {
+    echo 3;
+}
+
+if (3 <= $a) {
+    echo 3;
+}
+
+if (5 > $a) {
+    echo 5;
+}
+
+if (null < $a) {
+    echo "null";
+}
--- tests/codestyle/fixtures/yodaStyle/expected.php
+++ tests/codestyle/fixtures/yodaStyle/expected.php
@@ -5,3 +5,23 @@ declare(strict_types=1);
 if ($a === null) {
     echo "null";
 }
+
+if ($a === null) {
+    echo "null";
+}
+
+if ($a <= 3) {
+    echo 3;
+}
+
+if ($a >= 3) {
+    echo 3;
+}
+
+if ($a < 5) {
+    echo 5;
+}
+
+if ($a > null) {
+    echo "null";
+}
