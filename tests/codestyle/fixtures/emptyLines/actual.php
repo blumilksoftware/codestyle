cc9ca1ad57903438f1231599b379e7e5768c13d8
Krzysztof Rewak <krzysztof.rewak@blumilk.pl>
2023-04-04 10:28:50 +0200

    - common ruleset update (#90)
    * - common ruleset update
    
    * - common ruleset update

---
+++ tests/codestyle/fixtures/emptyLines/actual.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    "default" => env("DB_CONNECTION", "mysql"),
+
+
+    "connections" => [
+        "sqlite" => [
+            "driver" => "sqlite",
+            "url" => env("DATABASE_URL"),
+            "database" => env("DB_DATABASE", database_path("database.sqlite")),
+            "prefix" => "",
+            "foreign_key_constraints" => env("DB_FOREIGN_KEYS", true),
+        ],
+    ]
+];
