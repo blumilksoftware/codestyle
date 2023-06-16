354c5eda936859ef7b52df0c7969b929438b6ace
Kamil <kamil.piech.97@gmail.com>
2023-06-16 07:59:28 +0200

    #94 - added blank line fixer

---
+++ tests/codestyle/fixtures/blankLineBeforeStatement/actual.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+class BlankLine
+{
+    public function getFoo(int $foo): int {
+        $bar = $foo;
+        return $bar;
+    }
+}
---
+++ tests/codestyle/fixtures/blankLineBeforeStatement/expected.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+class BlankLine
+{
+    public function getFoo(int $foo): int {
+        $bar = $foo;
+
+        return $bar;
+    }
+}
