0e61b4f7757b0b636fce5e8c8d1d4434fce22dbd
Kamil <kamil.piech.97@gmail.com>
2023-11-20 07:52:36 +0100

    #105 - extended tests

--- tests/fixtures/compactArray/actual.php
+++ tests/fixtures/compactArray/actual.php
@@ -11,4 +11,16 @@ class CompactArray
     protected $other = [
 
         ];
+    protected $commentWithValue = [
+        // \App\Http\Middleware\TrustHosts::class,
+        123
+    ];
+    protected $comment = [
+        // \App\Http\Middleware\TrustHosts::class,
+    ];
+    /**
+     * TODO: example todo
+     */
+    protected $doc = [
+    ];
 }
--- tests/fixtures/compactArray/expected.php
+++ tests/fixtures/compactArray/expected.php
@@ -7,4 +7,12 @@ class CompactArray
     protected $except = [];
     protected $excepted = [];
     protected $other = [];
+    protected $commentWithValue = [
+        // \App\Http\Middleware\TrustHosts::class,
+        123,
+    ];
+    protected $comment = [];
+
+    /** TODO: example todo */
+    protected $doc = [];
 }
