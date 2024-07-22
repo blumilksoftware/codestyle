f645dcaf191ba2cf21b34771c1610d9fde22d8d1
Kamil <kamil.piech.97@gmail.com>
2024-07-22 13:25:40 +0200

    #112 - fix: csf

--- src/Fixers/ClassKeywordFixer.php
+++ src/Fixers/ClassKeywordFixer.php
@@ -8,9 +8,7 @@ use PhpCsFixer\Fixer\FixerInterface;
 use PhpCsFixer\FixerDefinition\CodeSample;
 use PhpCsFixer\FixerDefinition\FixerDefinition;
 use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
-use PhpCsFixer\Tokenizer\CT;
 use PhpCsFixer\Tokenizer\Tokens;
-use PhpCsFixerCustomFixers\Fixer\NoCommentedOutCodeFixer;
 use SplFileInfo;
 
 final class ClassKeywordFixer implements FixerInterface
@@ -18,7 +16,7 @@ final class ClassKeywordFixer implements FixerInterface
     public function getDefinition(): FixerDefinitionInterface
     {
         return new FixerDefinition(
-            'Converts FQCN strings to `*::class` keywords.',
+            "Converts FQCN strings to `*::class` keywords.",
             [
                 new CodeSample(
                     '<?php
@@ -26,11 +24,11 @@ final class ClassKeywordFixer implements FixerInterface
 $foo = \'PhpCsFixer\Tokenizer\Tokens\';
 $bar = "\PhpCsFixer\Tokenizer\Tokens";
 $baz = "\Exception";
-'
+',
                 ),
             ],
-            'This rule does not have an understanding of whether a class exists in the scope of the codebase or not, relying on run-time and autoloaded classes to determine it, which makes the rule useless when running on a single file out of codebase context.',
-            'Do not use it, unless you know what you are doing.'
+            "This rule does not have an understanding of whether a class exists in the scope of the codebase or not, relying on run-time and autoloaded classes to determine it, which makes the rule useless when running on a single file out of codebase context.",
+            "Do not use it, unless you know what you are doing.",
         );
     }
 
@@ -49,10 +47,10 @@ $baz = "\Exception";
         for ($index = $tokens->count() - 1; $index >= 0; --$index) {
             $token = $tokens[$index];
 
-            if ($token->isGivenKind(T_CONSTANT_ENCAPSED_STRING) && (strpos($token->getContent(), '\\') !== false)) {
+            if ($token->isGivenKind(T_CONSTANT_ENCAPSED_STRING) && (strpos($token->getContent(), "\\") !== false)) {
                 $name = substr($token->getContent(), 1, -1);
-                $name = ltrim($name, '\\');
-                $name = str_replace('\\\\', '\\', $name);
+                $name = ltrim($name, "\\");
+                $name = str_replace("\\\\", "\\", $name);
 
                 if ($this->exists($name)) {
                     $substitution = Tokens::fromCode("<?php echo \\{$name}::class;");
@@ -65,17 +63,6 @@ $baz = "\Exception";
                 }
             }
         }
-    }
-
-    private function exists(string $name): bool
-    {
-        if (class_exists($name) || interface_exists($name) || trait_exists($name)) {
-            $rc = new \ReflectionClass($name);
-
-            return $rc->getName() === $name;
-        }
-
-        return false;
     }
 
     public function getName(): string
@@ -91,5 +78,16 @@ $baz = "\Exception";
     public function supports(SplFileInfo $file): bool
     {
         return true;
+    }
+
+    private function exists(string $name): bool
+    {
+        if (class_exists($name) || interface_exists($name) || trait_exists($name)) {
+            $rc = new \ReflectionClass($name);
+
+            return $rc->getName() === $name;
+        }
+
+        return false;
     }
 }
