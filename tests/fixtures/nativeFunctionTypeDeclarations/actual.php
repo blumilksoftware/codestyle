13964703563049b36b010b9f97b0b5c712b8a7ba
krzysztofkozyra021 <cysiuu021@protonmail.com>
2025-08-22 11:09:58 +0200

    Native function type declarations

---
+++ tests/fixtures/nativeFunctionTypeDeclarations/actual.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+enum Status: String
+{
+    case ACTIVE = "active";
+}
+enum Priority: Int
+{
+    case HIGH = 1;
+}
---
+++ tests/fixtures/nativeFunctionTypeDeclarations/expected.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+enum Status: string
+{
+    case ACTIVE = "active";
+}
+enum Priority: int
+{
+    case HIGH = 1;
+}
